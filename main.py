8693badf6b0b7b8c4221a5666a01c538e79cacec
euny0ung <56199780+euny0ung@users.noreply.github.com>
2022-11-05 02:26:13 +0900

    feat: Update logfile - 입력값을 로그파일에 연속으로 저장하는 기능 - 정상입력값을 log.txt에 저장 - 비정상 입력값을 errorLog.txt에 저장

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 # Program make a simple calculator
 
+from io import StringIO
+
 # This function adds two numbers
 def add(x, y):
-    return x + y
+    return x+y
 
 # This function subtracts two numbers
 def subtract(x, y):
-    return x - y
+    return x-y
 
 
 # This function multiplies two numbers
 def multiply(x, y):
-    return x * y
+    return x*y
 
 #Need to define divide function.
 def divide (x,y):
@@ -21,8 +23,22 @@ def divide (x,y):
     except ZeroDivisionError:
         print("You can't divide it by zero")
         return None
-   
 
+def Log(newLog):
+    f=open("./log.txt", "a")
+    f.write(newLog)
+    f.close()
+    
+def errorLog(newLog):
+    f=open("./errorLog.txt", "a")
+    f.write(newLog)
+    f.close()
+    
+def returnPrint(*message):
+    io=StringIO()
+    print(*message, file=io)
+    return io.getvalue()
+    
 print("Calculator started.")
 
 print("Select operation.")
@@ -35,6 +51,8 @@ print("4.Divide")
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
+    
+    abnormal=False;
 
     # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
@@ -42,32 +60,62 @@ while True:
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            operator="+"
+            log=returnPrint(num1, operator, num2, "=", add(num1, num2))
+            print(log)
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            operator="-"
+            log=returnPrint(num1, operator, num2, "=", subtract(num1, num2))
+            print(log)
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            operator="*"
+            log=returnPrint(num1, operator, num2, "=", multiply(num1, num2))
+            print(log)
             
         elif choice =='4':
+            operator="/"
             res=divide(num1, num2)
-            if res is not None:
-                print(num1, "/", num2, "=", res)
-            
+            if res is not None: 
+                log=returnPrint(num1, operator, num2, "=", res)
+                print(log)
+            else:
+                log=returnPrint(num1, operator, num2, "=", res)
+                abnormal=True 
+                
+        if abnormal:
+            errorLog(log)
+        else:
+            Log(log)
 
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ").lower();
+        
+        # if next_calculation == "no":
+        #     answer=input("Are you sure? (yes/no): ").lower();
+        #     if answer == "yes":
+        #         break;
+        #     elif answer=="no":
+        #         continue;
+        # elif next_calculation=="yes":
+        #     continue;
+        # else:
+        #     print("You can only choose yes or no")
+        #     # 재확인 루틴 개발중
+        
         if next_calculation == "no":
             answer=input("Are you sure? (yes/no): ").lower();
             if answer == "yes":
                 break;
             if answer=="no":
-                continue;
+                continue; 
 
     else:
         print("Invalid Input")
+        log=returnPrint(choice)
+        errorLog(log) ###개행 들어가야함
 
 
 
